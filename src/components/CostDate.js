713d4e6deb29ba54c962eea1dd96635f561ce994
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-04-30 19:54:33 +0300

    +

--- src/components/CostDate.js
+++ src/components/CostDate.js
@@ -1,12 +1,13 @@
+import styles from "./CostDate.module.css";
 function CostDate({ date }) {
   const month = date.toLocaleString("en-US", { month: "long" });
   const year = date.getFullYear();
   const day = date.toLocaleString("en-US", { day: "2-digit" });
   return (
-    <div>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+    <div className={styles.costDate}>
+      <div className={styles.costDate}>{month}</div>
+      <div className={styles.costDateYear}>{year}</div>
+      <div className={styles.costDateDay}>{day}</div>
     </div>
   );
 }
