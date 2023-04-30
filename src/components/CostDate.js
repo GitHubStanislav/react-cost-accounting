0d577149d057a6da64a78e8f4f752d4de477c1aa
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-04-30 19:16:05 +0300

    Cost Date new component

---
+++ src/components/CostDate.js
@@ -0,0 +1,14 @@
+function CostDate({ date }) {
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const year = date.getFullYear();
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  return (
+    <div>
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
+    </div>
+  );
+}
+
+export default CostDate;
--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,15 +1,9 @@
 import styles from "./CostItem.module.css";
+import CostDate from "./CostDate";
 function CostItem({ date, description, amount }) {
-  const month = date.toLocaleString("en-US", { month: "long" });
-  const year = date.getFullYear();
-  const day = date.toLocaleString("en-US", { day: "2-digit" });
   return (
     <div className={styles.costItem}>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <CostDate date={date} />
       <div className={styles.costItem__description}>
         <h2>{description}</h2>
         <div className={styles.costItem__price}>{amount}</div>
